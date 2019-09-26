aa7e6bb42ca2d23a68a52edca0b3067c7492d567
Akis Kourouklis <48292762+AkisKourouklis@users.noreply.github.com>
2019-09-26 16:44:01 +0300

    admin sidebar update

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,31 @@
 var express = require("express");
 var router = express.Router();
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user')
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var User = require("../models/user");
 
 var head = [
   { name: "Bootstrap_CSS", url: "/css/bootstrap.min.css", css: true },
   { name: "Bootstrap_JS", url: "/js/bootstrap.min.js", js: true }
 ];
 
-passport.serializeUser(function (user, done) {
-	done(null, user.id);
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-	User.getUserById(id, function (err, user) {
-		done(err, user);
-	});
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
-router.post('/login',
-	passport.authenticate('local', { successRedirect: '/admin', failureRedirect: '/admin/login', failureFlash: true }),
-	function (req, res) {
-		res.redirect('/admin');
-});
+router.post(
+  "/login",
+  passport.authenticate("local", { successRedirect: "/admin", failureRedirect: "/admin/login", failureFlash: true }),
+  function(req, res) {
+    res.redirect("/admin");
+  }
+);
 
 router.get("/login", (req, res) => {
   res.render("login", { head });
@@ -33,40 +35,41 @@ router.get("/", ensureAuthenticated, (req, res) => {
   res.render("admin", { head });
 });
 
-function createUser(name, username, password, callback){
-    var newUser = User({
-        username,
-        password,
-        name
-    })
-    User.createUser(newUser, function(err, user){
-        callback(user)
-    })
+function createUser(name, username, password, callback) {
+  var newUser = User({
+    username,
+    password,
+    name
+  });
+  User.createUser(newUser, function(err, user) {
+    callback(user);
+  });
 }
 
-passport.use(new LocalStrategy(
-	function (username, password, done) {
-		User.getUserByUsername(username, function (err, user) {
-			if (err) throw err;
-			if (!user) {
-				return done(null, false, { message: 'Unknown user' });
-			}
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.getUserByUsername(username, function(err, user) {
+      if (err) throw err;
+      if (!user) {
+        return done(null, false, { message: "Unknown user" });
+      }
 
-			User.comparePassword(password, user.password, function (err, isMatch) {
-				if (err) throw err;
-				if (isMatch) {
-					return done(null, user);
-				} else {
-					return done(null, false, { message: 'Wrong password' });
-			}
-		});
-	});
-}));
+      User.comparePassword(password, user.password, function(err, isMatch) {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Wrong password" });
+        }
+      });
+    });
+  })
+);
 
-router.get('/logout',ensureAuthenticated, function (req, res) {
-	req.logout();
-	req.flash('success_msg', 'You are logged out');
-	res.redirect('/');
+router.get("/logout", ensureAuthenticated, function(req, res) {
+  req.logout();
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/");
 });
 
 function ensureAuthenticated(req, res, next) {
