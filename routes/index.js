b716e162f36132b5f64602a0070f433977d7894c
Akis Kourouklis <48292762+AkisKourouklis@users.noreply.github.com>
2019-09-26 15:08:39 +0300

    404_redirect

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
 var express = require("express");
 var router = express.Router();
-var options = require("../models/options")
-var pages = require("../models/page")
+var options = require("../models/options");
+var pages = require("../models/page");
 
-router.get('/:page', (req, res) => {
-	pages.getPageByURL(req.params.page, (err, page)=>{
-		if(err) throw err;
-		if(!page){
-			res.render('stock_404')
-		}else{
-			options.getOptionsByName("menu", (err, menu) => {
-				res.render('stock', {menu, text: "hello world"});
-			});
-		}
-	})
-});	
+router.get("/:page", (req, res) => {
+  pages.getPageByURL(req.params.page, (err, page) => {
+    if (err) throw err;
+    if (!page) {
+      res.render("stock_404");
+    } else {
+      options.getOptionsByName("menu", (err, menu) => {
+        res.render("stock", { menu, text: "hello world" });
+      });
+    }
+  });
+});
 
-router.get('/', (req, res) => {
-	res.redirect('/home')
-});	
+router.get("/", (req, res) => {
+  res.redirect("/home");
+});
 
 module.exports = router;
