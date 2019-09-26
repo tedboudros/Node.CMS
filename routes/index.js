89507a01413b3ffcd15ffe85b11b7768d99c127f
Akis Kourouklis <48292762+AkisKourouklis@users.noreply.github.com>
2019-09-26 14:47:41 +0300

    test

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var options = require("../models/options")
+var options = require("../models/options");
 
-router.get('/', (req, res) => {
-    options.getOptionsByName("menu", (err, menu) => {
-        res.render('index', {menu, text: "hello world"});
-    });
+router.get("/", (req, res) => {
+  options.getOptionsByName("menu", (err, menu) => {
+    res.render("index", { menu, text: "hello world fuck you" });
+  });
 });
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		req.flash('error','Δεν έχετε κάνει είσοδο στο σύστημα.');
-		res.redirect('/login');
-	}
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.flash("error", "Δεν έχετε κάνει είσοδο στο σύστημα.");
+    res.redirect("/login");
+  }
 }
 
 module.exports = router;
